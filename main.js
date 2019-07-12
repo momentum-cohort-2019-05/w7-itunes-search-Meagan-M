a2c5c969a5a1fb5f8879ba10d72a813089d54eb8
Meagan-M <meaganmakinson80@gmail.com>
2019-07-12 17:12:43 -0400

    remove form in html, add display of songs and audio player

--- main.js
+++ main.js
@@ -1,77 +1,41 @@
-function q (sel) {
-    return document.querySelector(sel)
-  }
+let button = document.querySelector('#submit')
+let input = document.querySelector('#search')
+let output = document.querySelector('#output')
+let cors = 'https://cors-anywhere.herokuapp.com/'
+
+button.addEventListener('click', (e) => {
+    fetch(cors + 'https://itunes-api-proxy.glitch.me/search?term='+input.value)
+    
+    .then(response => response.json())
+    .then(function (data) {
+        console.log(data)
+        let finalHTML = ''
+
+        data.results.forEach( song => {
+            finalHTML += `
+            <div class ="col s4 m4 l4"> 
+            <div class="card" style="width: 18rem;">
+            <img src="${song.artworkUrl60}" 
+            class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${song.artistName}</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Listen</a>
+              
+           
+            <audio controls src="${song.previewUrl}">
+            Your browser does not support the
+            <code>audio</code> element.
+            </audio>
+            
+            </div>
+          </div>
+          </div>
+            
+            `
+    })
   
-function qs (sel) {
-    return document.querySelectorAll(sel)
-  }
-
-
-function displayMusicData (data) {
-const songList = document.createElement('div')
-songList.classList.add('planet', 'ba', 'bw2', 'purple')
-songList.innerHTML = `
-    <h3>${artist.name}</h3>
-    <p>Population: ${artist.song}</p>
-    <p>Climate: ${artist.album}</p>
-    <button class="get-song-data" data-url="${song.url}">Find out more about ${artist.name}</button>
-`
-return songList
-}
-// code for player
-// function displayMusicData (results) {
-// fetch(results)
-//     .then(res => res.json())
-//     .then(function (data) {
-//     const dataDisplay = q('#artist')
-//     dataDisplay.innerHTML = `
-//         <h3>More info about ${data.artist}</h3>
-//         <dl>
-//         <dt>album</dt>
-//         <dd>${data.album}</dd>
-//         </dl>
-//     `
-//     })
-// }
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('https://itunes-api-proxy.glitch.me/search?term=Radiohead&entity=song')
-// returns the data to you as a JS object
-        .then(function (response) {
-// return response.json also returns a promise
-            return response.json()
-        })
-// takes JS object and does things with it
-        .then(function(data) {
-           console.log(data);
-             //return 'something here'
-        })
+     output.innerHTML = finalHTML 
+  })
+.catch( error => console.log(error))
 })
-// submits artist input when search button is pressed. preventDefault stops the page from reloading when the submit button is pressed. 
-// document.querySelector('#artist').addEventListener('click', function(event.preventDefault()))
- 
-/*************************************************************************
- * ******* getting information from submit field
- * 
- * 
- * document.addEventListener('DOMContentLoaded', function() {
- *  q('form).addEventListener('submit'), function (event) {
-    * event.preventDefault()
-    * const searchTerm = q('#submit').value
-    * const url = '}'
- * 
- *  fetch(url)
- *      .then(response => response.json())
- *      .then(function (data) {
- *          resultsDiv.innerHTML = ''
- *          for (let )
- * }
- * }})
- * 
- * function planetNode in examples  will display selected information when a search is submitted. 
- * 
- * 
- */
-
-// data.results
