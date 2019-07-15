ae8b2b30fd85f9248e048635d2b40de81ba19784
Meagan-M <meaganmakinson80@gmail.com>
2019-07-15 09:45:26 -0400

    fix audio player so that there is just one and not one for each song

--- main.js
+++ main.js
@@ -3,39 +3,42 @@ let input = document.querySelector('#search')
 let output = document.querySelector('#output')
 let cors = 'https://cors-anywhere.herokuapp.com/'
 
+function handleClick(e) {
+    document.querySelector('.player').src=e.target.dataset['url'];
+    document.querySelector('.player').autoplay=true;
+    document.querySelector('.currentSong').innerHTML=e.target.dataset['songname'];
+}
+
 button.addEventListener('click', (e) => {
     fetch(cors + 'https://itunes-api-proxy.glitch.me/search?term='+input.value)
     
     .then(response => response.json())
     .then(function (data) {
-        console.log(data)
         let finalHTML = ''
 
         data.results.forEach( song => {
             finalHTML += `
             <div class ="col s4 m4 l4"> 
             <div class="card" style="width: 18rem;">
-            <img src="${song.artworkUrl60}" 
+            <img src="${song.artworkUrl100}" 
             class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${song.artistName}</h5>
+              <span>${song.trackName}</span>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Listen</a>
-              
-           
-            <audio controls src="${song.previewUrl}">
-            Your browser does not support the
-            <code>audio</code> element.
-            </audio>
-            
+              <button onClick="handleClick(event)" class="btn btn-primary" data-songname="${song.trackName}" data-url="${song.previewUrl}">Listen</button>                        
             </div>
           </div>
           </div>
-            
-            `
+       `
     })
-  
-     output.innerHTML = finalHTML 
+    
+     output.innerHTML = finalHTML;
+    //  document.querySelector('.btn.btn-primary').addEventListener('click', function(e) {
+    //     console.log('clicked');
+    //     console.log(e.target.dataset['url']);
+    //     document.querySelector('.player').src=e.target.dataset['url'];
+    // });
   })
 .catch( error => console.log(error))
 })
